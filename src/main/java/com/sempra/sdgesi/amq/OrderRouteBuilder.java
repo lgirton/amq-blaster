5f9c2c959c0b60ef3c8b5e753504b7037a4c2f6f
Your Name <you@example.com>
2020-01-23 15:23:28 -0800

    first commit

---
+++ src/main/java/com/sempra/sdgesi/amq/OrderRouteBuilder.java
@@ -0,0 +1,29 @@
+package com.sempra.sdgesi.amq;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("activemq:queue:orders")
+            .to("log:?level=INFO&showBody=true");
+
+        restConfiguration()
+            .component("spark-rest")
+            .bindingMode(RestBindingMode.json)
+            .port(9091);
+
+        rest("/order")
+            .post()
+                .to("activemq:queue:orders")
+            .get("/{id}")
+                .to("log:?level=INFO&showBody=true");
+
+    }
+
+}
